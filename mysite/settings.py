ef3a17b74036208ab74d86414455ab26541833f3
yulia <julie_gal@mail.ru>
2017-02-11 00:42:26 +0300

    Modified templates to display posts from database.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bctdcr-qu27_n&2940yj^j43o1^^9$&y(@5@6$ukvz!7m&uw7s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['juliegal.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
